f44135921bed93b88ebd90341877c95b1c1808ca
Mio <gusw.0246.lpsyg@gmail.com>
2024-05-16 14:57:27 +0200

    made a file to do second exercise

---
+++ 02_MathTime/datatype.py
@@ -0,0 +1,15 @@
+#introduce data types
+print("Python has three numeric types: int, float, and complex.")
+
+#Creat a variable or variables
+
+myValue = 10000000000
+
+#print myValue
+print(myValue)
+
+#and print type of myValue
+print(type(myValue))
+
+#let's combine strings and numbers
+print(str(myValue), "is of the data type", str(type(myValue)))
