7e4b30098dc4cf683783520c19cc7172b1006076
Mio <gusw.0246.lpsyg@gmail.com>
2024-05-16 18:49:35 +0200

    2nd task done

--- 02_MathTime/datatype.py
+++ 02_MathTime/datatype.py
@@ -13,3 +13,24 @@ print(type(myValue))
 
 #let's combine strings and numbers
 print(str(myValue), "is of the data type", str(type(myValue)))
+
+#Exercise 3 print float data type
+floatNumber = 3.14
+floatType = type(floatNumber)
+print(str(floatNumber), "is of the data type", str(floatType))
+
+#Exercise 4 print complex data type
+complexData = 5j
+complexType = type(complexData)
+print(str(complexData), "is of the data type", str(complexType))
+
+#Excercise 5 print bool data type
+myValue = True
+myValue_type = type(myValue)
+print(str(myValue), "is of the data type", str(myValue_type))
+
+second_value = False
+print(second_value)
+print(type(second_value))
+
+
