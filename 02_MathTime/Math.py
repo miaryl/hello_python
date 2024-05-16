3096f9bfc83824114e6f3b831821cb4d275705f9
Mio <gusw.0246.lpsyg@gmail.com>
2024-05-16 14:46:26 +0200

    lab's second tarea, math

---
+++ 02_MathTime/Math.py
@@ -0,0 +1,22 @@
+#Let's make some story with numbers
+story = """John likes chocolate and every day buys 10 tablets,
+      so, how many chocolate buys in a week?
+      hopefully, he don't die early!!"""
+print(story)
+ask_answer = input("put how many John buys in a week?: ")
+print(input)
+#better to check the answer is correct or not, i guess
+if ask_answer == 70:
+    print("Wow, let you eat chocolate!")
+else:
+    print("Better to use your fingers or calculater")
+
+#just tired put many words, let's do simple math practice
+#print the result of 8 - 6
+print(8 - 6)
+
+#print 4 * 4 also?
+print(4 * 4)
+
+#so better to divide 
+print(8 / 2)
