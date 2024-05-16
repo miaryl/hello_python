ce6125d4249a42c8cfbd03352d5a659f42366f3f
Mio <gusw.0246.lpsyg@gmail.com>
2024-05-16 14:31:47 +0200

    ask name and greetings

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 print("Hello World!!")
 print("Hellooooo")
 print("enter shikari")
+
+#let's do something more, like ask name and do greeting
+name =input("Can you have your name?: ")
+print("Hello", name)
