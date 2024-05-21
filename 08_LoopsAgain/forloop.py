faa51da317ceeae2352adc00eeae6613648ad236
Mio <gusw.0246.lpsyg@gmail.com>
2024-05-21 10:38:59 +0200

    Task 8 is done

---
+++ 08_LoopsAgain/forloop.py
@@ -0,0 +1,7 @@
+# use for loop and count 10
+print("count to 10")
+
+for i in range (1,11):
+    print(i)
+
+print("hide and seek is ready......!")
---
+++ 08_LoopsAgain/oopsloops.py
@@ -0,0 +1,25 @@
+#print the game rules! 
+print("WELCOME TO GUESS THE NUMBER!!!")
+print("""
+      The rules are simple. 
+      I will think of a number, 
+      and you will try to guess it ;)!
+      """)
+
+# import random 
+import random
+
+# male valuable to choose a number from 1 to 99
+number = random.randint(1,99)
+
+#Track whether the user guessed your number by creating a variable called guessRight
+guessRight = False
+
+# while loop 
+while guessRight != True:
+    guess = input("Guess a number between 1 and 99: ")
+    if int(guess) == number:
+        print("You guessed {}. That is correct! You win!".format(guess))
+        guessRight = True
+    else:
+        print("You guessed {}. Sorry, that isn’t it. Try again.".format(guess))
