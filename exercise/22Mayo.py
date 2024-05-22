f6755e63a7b217a2a04291e4148724adc33b6f17
Mio <gusw.0246.lpsyg@gmail.com>
2024-05-22 23:54:47 +0200

    repado update

--- exercise/22Mayo.py
+++ exercise/22Mayo.py
@@ -1,4 +1,35 @@
-# 
+# for loops challenge
+hijos = [ "juan", "Marta", "Laura", "Pedro"]
+
+for hijo in hijos:
+    print(hijo, "se está haciendo la cama")
+
+# Range 
+for numero in range(4):
+    print(hijos[numero])
+
+for numero in range(len(hijos)):
+    print(numero)
+
+# range(start,end,...)
+for numero in range(2,10,2):
+    print(numero)
+
+# while loops -> mientras pasa A, hagamos B
+lluvia = True
+horas = 0 
+
+while lluvia == True:
+    print("Usamos paraguas")
+    horas += 1
+    if horas == 5 :
+        lluvia = False
+if lluvia == True:
+    print("El suelo está mojado")
+
+print("Hace sol")
+
+# add all the numbers in show the result
 numeros = [25, 55, 98, 10]
 
 total_num = 0
@@ -9,5 +40,7 @@ for numero in numeros:
 # if only want total number, print() should be out of for loop
 print(total_num)
 
-sumalista = sum(numeros)
+
+# Or sum() is more easy
+suma_lista = sum(numeros)
 print(total_num)
