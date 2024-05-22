906f2a71372a0adcd96008ab4bf3785cb9b6f9c8
Mio <gusw.0246.lpsyg@gmail.com>
2024-05-22 14:54:45 +0200

    add today's exercise

---
+++ exercise/22Mayo.py
@@ -0,0 +1,13 @@
+# 
+numeros = [25, 55, 98, 10]
+
+total_num = 0
+
+for numero in numeros:
+    total_num += numero
+# if i write print() in for loop, show each result of calculation
+# if only want total number, print() should be out of for loop
+print(total_num)
+
+sumalista = sum(numeros)
+print(total_num)
